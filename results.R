f691fd4bdd0ccea27760e45a1c44d77b27795ff0
Christoph Safferling <christoph.safferling@bluebyte.de>
2016-08-31 16:06:06 +0200

    stringsAsFactors &%$%§

--- results.R
+++ results.R
@@ -22,7 +22,7 @@ events <- read_html(bbc.rio2016) %>%
   # turn into full bbc link
   paste0("http://www.bbc.com", .) %>% 
   # turn into proper dataframe
-  as.data.frame() %>% 
+  as.data.frame(stringsAsFactors = FALSE) %>% 
   setNames("link") %>% 
   # get sport and event explicitly
   mutate(
@@ -33,17 +33,42 @@ events <- read_html(bbc.rio2016) %>%
 # 31 events
 unique(events$sport)
 
+knockout.results <- c("archery", "badminton", "basketball", 
+                      "boxing", "fencing", "handball", "hockey", 
+                      "judo", "rugby-sevens", "table-tennis", "taekwondo", 
+                      "tennis", "volleyball", "water-polo", "wrestling")
 
 
-sw <- "http://www.bbc.com/sport/olympics/rio-2016/results/sports/swimming/womens-800m-freestyle"
-sw.html <- read_html(sw)
 
-dta <- sw.html %>%
-  # get first <div> after <h3>Final</h3>
-  html_node(xpath = "//h3[contains(., 'Final')]/following-sibling::div") %>% 
-  # get table from div
-  html_node("table") %>% 
-  # convert into dataframe
-  html_table()
+
+
+
+#sw <- "http://www.bbc.com/sport/olympics/rio-2016/results/sports/swimming/womens-800m-freestyle"
+
+f.get.bbc.nonknockout.results <- function(link){
+  dta <- read_html(link) %>%
+    # get first <div> after <h3>Final</h3>
+    html_node(xpath = "//h3[contains(., 'Final')]/following-sibling::div") %>% 
+    # get table from div
+    html_node("table") %>% 
+    # convert into dataframe
+    html_table() %>% 
+    mutate(
+      sport = events[events$link == link, "sport"],
+      event = events[events$link == link, "event"]
+    )
+}
+
+
+dta <- list()
+
+for(i in events %>% filter(!sport %in% knockout.results) %>% .$link){
+  dta[[i]] <- f.get.bbc.nonknockout.results(i)
+}
+
+
+
+
+
 
 
